fcc3b7d4c99939cd72bdff769d4a637155ea17b5
Steavn Chen <tom9876555@yahoo.com.tw>
2021-12-17 21:37:54 +0800

    test33 addlike and unlike

--- services/userService.js
+++ services/userService.js
@@ -290,66 +290,6 @@ const userService = {
       callback({ status: "success", message: "all replies killed" });
     });
   },
-  // addLike: (req, res, callback) => {
-  //   Like.findOne({
-  //     where: {
-  //       UserId: helpers.getUser(req).id,
-  //       TweetId: req.params.id,
-  //     },
-  //   })
-  //     .then((like) => {
-  //       if (!like) {
-  //         return Like.create({
-  //           UserId: helpers.getUser(req).id,
-  //           TweetId: req.params.id,
-  //           isLike: true,
-  //         }).then((like) => {
-  //           return callback({ status: "success", message: "喜歡此筆推文。" });
-  //         });
-  //       }
-  //       if (like.isLike === false) {
-  //         return like.update({ ...like, isLike: !like.isLike }).then((like) => {
-  //           return callback({ status: "success", message: "喜歡此筆推文。" });
-  //         });
-  //       }
-  //       return callback({
-  //         status: "error",
-  //         message: "錯誤 ! 此筆推文己喜歡。",
-  //       });
-  //     })
-  //     .catch((err) => console.log(err));
-  // },
-  removeLike: (req, res, callback) => {
-    Like.findOne({
-      where: {
-        UserId: helpers.getUser(req).id,
-        TweetId: req.params.id,
-      },
-    })
-      .then((like) => {
-        if (!like) {
-          return Like.create({
-            UserId: helpers.getUser(req).id,
-            TweetId: req.params.id,
-            isLike: false,
-          }).then((like) => {
-            return callback({ status: "success", message: "此筆推文取消喜歡" });
-          });
-        } else if (like.isLike === true) {
-          return like.destroy().then((like) => {
-            return callback({ status: "success", message: "此筆推文取消喜歡" });
-          });
-        } else {
-          return like.destroy().then((like) => {
-            return callback({
-              status: "error",
-              message: "錯誤 ! 此筆推文己取消喜歡。",
-            });
-          });
-        }
-      })
-      .catch((err) => console.log(err));
-  },
   addLike: (req, res, callback) => {
     return Like.findOrCreate({
       where: {
@@ -368,23 +308,55 @@ const userService = {
       .catch((err) => console.log(err));
   },
   // removeLike: (req, res, callback) => {
-  //   return Like.findOne({
+  //   Like.findOne({
   //     where: {
   //       UserId: helpers.getUser(req).id,
   //       TweetId: req.params.id,
   //     },
   //   })
   //     .then((like) => {
-  //       // if (!like) {
-  //       //   return callback({ status: "success", message: "刪除此筆推文" });
-  //       // } else {
-  //         like.destroy().then((like) => {
-  //           return callback({ status: "success,", message: "刪除此筆推文" });
+  //       if (!like) {
+  //         return Like.create({
+  //           UserId: helpers.getUser(req).id,
+  //           TweetId: req.params.id,
+  //           isLike: false,
+  //         }).then((like) => {
+  //           return callback({ status: "success", message: "此筆推文取消喜歡" });
   //         });
-  //       // }
+  //       } else if (like.isLike === true) {
+  //         return like.destroy().then((like) => {
+  //           return callback({ status: "success", message: "此筆推文取消喜歡" });
+  //         });
+  //       } else {
+  //         return like.destroy().then((like) => {
+  //           return callback({
+  //             status: "error",
+  //             message: "錯誤 ! 此筆推文己取消喜歡。",
+  //           });
+  //         });
+  //       }
   //     })
   //     .catch((err) => console.log(err));
   // },
+
+  removeLike: (req, res, callback) => {
+    return Like.findOne({
+      where: {
+        UserId: helpers.getUser(req).id,
+        TweetId: req.params.id,
+      },
+    })
+      .then((like) => {
+        if (!like) {
+          return callback({ status: "success", message: "取消喜歡推文" });
+        } else {
+          like.destroy().then((like) => {
+            return callback({ status: "success,", message: "取消喜歡推文" });
+          });
+        }
+      })
+      .catch((err) => console.log(err));
+  },
 
   profileUser: async (req, res, callback) => {
     try {
